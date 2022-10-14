957ada04b08d80df9840f03e3873f9a781c986c3
Jaypee1414 <jhnplbncyrn03@gmail.com>
2022-10-14 12:06:24 +0800

    change color caret

--- main.js
+++ main.js
@@ -21,7 +21,7 @@ window.addEventListener('load', ()=>{
         content.classList.add("content");
 
         task.appendChild(content);
-        
+
         const list_task = document.createElement("input");
         list_task.classList.add("text");
         list_task.type = "text";
@@ -29,8 +29,40 @@ window.addEventListener('load', ()=>{
         list_task.setAttribute("readonly","readonly");
 
         content.appendChild(list_task);
-        task.appendChild(content);
+        task.appendChild(content)
+
+        const action = document.createElement("div");
+        action.classList.add("action");
+
+        const edit_btn = document.createElement("button");
+        edit_btn.classList.add("edit");
+        edit_btn.textContent = "Edit";
+
+        const delete_btn = document.createElement("button");
+        delete_btn.classList.add("delete");
+        delete_btn.textContent = "Delete";
+
+        action.appendChild(edit_btn);
+        action.appendChild(delete_btn);
+
+        task.appendChild(action);
         list.appendChild(task);
+
+
+        edit_btn.addEventListener('click',()=>{
+            if(edit_btn.lowerCase() == "edit"){
+                input.removeAttribute("readonly");
+                input.focus();
+                edit_btn.textContent = "Save";
+            }else{
+                input.setAttribute("readonly", "readonly");
+                edit_btn.textContent = "Edit"
+            }
+        })
+
+        delete_btn.addEventListener('click',()=>{
+            list.removeChild(task);
+        })
 
     })
 })
